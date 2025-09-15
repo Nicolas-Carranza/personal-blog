00baeb9303158bc9fd93a0b1367b576f76e9aeaf
Nicolas-Carranza <carranzanicolas3@gmail.com>
2025-09-15 09:19:31 +0100

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Navigation from '@/components/Navigation'
 import Footer from '@/components/Footer'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           </main>
           <Footer />
           <SpeedInsights />
+          <Analytics />
         </div>
       </body>
     </html>
